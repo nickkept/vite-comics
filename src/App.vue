16934979e137fd13d7dc8ef3219d74182369ebba
nickkept <nickit1899@gmail.com>
2022-12-02 17:22:02 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import 'bootstrap/dist/css/bootstrap.min.css'
+import MyNav from "./components/MyNav.vue";
+// import AppHero from "./components/AppHero.vue"
+export default {
+  components: {MyNav}
+}
+</script>
+
+<template>
+<MyNav></MyNav>
+</template>
+
+<style lang="scss">
+	@use "./styles/general.scss";
+  
+  
+</style>
---
+++ src/components/MyNav.vue
@@ -0,0 +1,45 @@
+<template>
+    <header>
+        <div class="container d-flex justify-content-between align-items-baseline">
+            <div>
+                <img src="/dc-logo.png" alt="">
+            </div>
+            <div>
+                <ul class="d-flex">
+                    <li v-for="(link) in links">
+                        <a href="">{{link}}</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </header>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            links: [
+            "CHARACTERS",
+            "COMICS",
+            "MOVIES",
+            "TV",
+            "GAMES",
+            "COLLECTIBLES",
+            "VIDEOS",
+            "FANS",
+            "NEWS",
+            "SHOP",
+        ],
+        };
+},
+methods: {},
+};
+</script>
+<style lang="scss">
+    ul{
+        list-style: none;
+        a{
+        text-decoration: none;
+    }
+    }
+</style>
